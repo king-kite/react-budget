42f5c5883fe359fe64cfe210bfce207f1d95f29e
kingkite <kitewebdevelopers@gmail.com>
2022-07-08 09:39:26 +0100

    Stable a bit

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,35 +1,53 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setBudgets } from "../store/features/budgets-slice";
 import { Cards, Top } from "../components/Home";
 import { BudgetCard } from "../components/Budgets";
+import { LoadingPage } from "../utils"
 
+const Dashboard = () => {
+	const [loading, setLoading] = useState(false)
 
-const Dashboard = () => (
-	<div>		
-		<Top />
-		<Cards />
-		<div>
-			<div className="flex flex-col items-start my-4 sm:flex-row sm:items-center sm:justify-between">
-				<div className="my-2">
-					<h3 className="top-heading">
-						Latest Budgets
-					</h3>
-					<p className="top-description">
-						Take a look at your lateset budgets and how your fair.
-					</p>
+	const dispatch  = useDispatch()
+	const budgets = useSelector(state => state.budgets.data)
+
+	useEffect(() => {
+		setLoading(true);
+		setTimeout(() => {
+			const budgets = localStorage.getItem("budgets");
+			if (budgets !== null) {
+				dispatch(setBudgets(JSON.parse(budgets)));
+			}
+			setLoading(false);
+		}, 2000);
+	}, [dispatch]);
+
+	return (
+		<div>		
+			<Top />
+			<Cards />
+			<div>
+				<div className="flex flex-col items-start my-4 sm:flex-row sm:items-center sm:justify-between">
+					<div className="my-2">
+						<h3 className="top-heading">
+							Latest Budgets
+						</h3>
+						<p className="top-description">
+							Take a look at your lateset budgets and how your fair.
+						</p>
+					</div>
+				</div>
+				<div className="gap-4 grid grid-cols-1 sm:gap-5 md:gap-6 md:grid-cols-2 lg:gap-8">
+					{budgets.slice(0, 3).map((budget, index) => (
+						<div key={index}>
+							<BudgetCard {...budget} />
+						</div>
+					))}
 				</div>
 			</div>
-			<div className="gap-4 grid grid-cols-1 sm:gap-5 md:gap-6 md:grid-cols-2 lg:gap-8">
-				<div>
-					<BudgetCard />
-				</div>
-				<div>
-					<BudgetCard />
-				</div>
-				<div>
-					<BudgetCard />
-				</div>
-			</div>
+			{loading & <LoadingPage />}
 		</div>
-	</div>
-);
+	);
+}
 
 export default Dashboard;
