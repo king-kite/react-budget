e754b70819c5b4c1756f0ca31eb3bf1ed32cf368
kingkite <kitewebdevelopers@gmail.com>
2022-07-08 20:32:58 +0100

    Adding Values To Home Cards

--- src/components/Home/Cards.jsx
+++ src/components/Home/Cards.jsx
@@ -5,20 +5,47 @@ import {
 	FaChartBar,
 } from "react-icons/fa";
 import { Card } from "../common"
+import { currencyFormatter } from "../../utils"
 
-const cards = [
-	{ Icon: FaCreditCard, iconBg: "bg-blue-100", iconColor: "text-blue-500", title: "spent so far", value: "0" },
-	{ Icon: FaUsers, iconBg: "bg-green-100", iconColor: "text-green-500", title: "total income", value: "0" },
-	{ Icon: FaDollarSign, iconBg: "bg-red-100", iconColor: "text-red-500", title: "total expenses", value: "0" },
-	{ Icon: FaChartBar, iconBg: "bg-purple-100", iconColor: "text-purple-500", title: "total budget", value: "0" }
-]
+const Cards = ({ budgets, budgetCount, expenses }) => {
 
-const Cards = () => (
-	<div className="gap-4 grid grid-cols-1 my-4 md:gap-6 md:grid-cols-2 lg:gap-3 lg:grid-cols-4 lg:my-7">
-		{cards.map((card, index) => (
-			<Card key={index} {...card}/>
-		))}
-	</div>
-)
+	const cards = [
+	{
+		Icon: FaDollarSign,
+		iconBg: "bg-red-100",
+		iconColor: "text-red-500",
+		title: "total expenses",
+		value: expenses > 0 ? currencyFormatter.format(expenses) : 0,
+	},
+	{
+		Icon: FaUsers,
+		iconBg: "bg-green-100",
+		iconColor: "text-green-500",
+		title: "total income",
+		value: "0",
+	},
+	{
+		Icon: FaCreditCard,
+		iconBg: "bg-blue-100",
+		iconColor: "text-blue-500",
+		title: "No. of budgets",
+		value: budgetCount,
+	},
+	{
+		Icon: FaChartBar,
+		iconBg: "bg-purple-100",
+		iconColor: "text-purple-500",
+		title: "total budget",
+		value: budgets > 0 ? currencyFormatter.format(budgets) : 0,
+	},
+];
+	return (
+		<div className="gap-4 grid grid-cols-1 my-4 md:gap-6 md:grid-cols-2 lg:gap-3 lg:grid-cols-4 lg:my-7">
+			{cards.map((card, index) => (
+				<Card key={index} {...card}/>
+			))}
+		</div>
+	)
+}
 
 export default Cards
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,24 +1,24 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 import { setBudgets } from "../store/features/budgets-slice";
 import { setExpenses } from "../store/features/expenses-slice";
 import { open } from "../store/features/modal-slice";
-import { BUDGETS_PAGE_URL, EXPENSES_PAGE_URL } from "../config"
+import { BUDGETS_PAGE_URL, EXPENSES_PAGE_URL } from "../config";
 import { Cards, Top } from "../components/Home";
 import { BudgetCard } from "../components/Budgets";
-import { ExpenseCard } from "../components/Expenses"
+import { ExpenseCard } from "../components/Expenses";
 import { Button } from "../components/controls";
-import { LoadingPage } from "../utils"
+import { LoadingPage } from "../utils";
 
 const Dashboard = () => {
-	const [loading, setLoading] = useState(false)
+	const [loading, setLoading] = useState(false);
 
-	const dispatch  = useDispatch()
-	const budgets = useSelector(state => state.budgets.data)
-	const expenses = useSelector(state => state.expenses.data)
+	const dispatch = useDispatch();
+	const budgets = useSelector((state) => state.budgets.data);
+	const expenses = useSelector((state) => state.expenses.data);
 
-	const navigate = useNavigate()
+	const navigate = useNavigate();
 
 	useEffect(() => {
 		setLoading(true);
@@ -29,23 +29,41 @@ const Dashboard = () => {
 			}
 			const expenses = localStorage.getItem("expenses");
 			if (expenses !== null) {
-				dispatch(setExpenses(JSON.parse(expenses)))
+				dispatch(setExpenses(JSON.parse(expenses)));
 			}
 			setLoading(false);
 		}, 2000);
 	}, [dispatch]);
 
 	return (
-		<div>		
+		<div>
 			<Top />
-			<Cards />
+			<Cards
+				expenses={
+					expenses && expenses.length > 0
+						? expenses.reduce(
+								(totalAmount, expense) =>
+									totalAmount + parseFloat(expense.amount),
+								0
+						  )
+						: 0
+				}
+				budgetCount={budgets ? budgets.length : 0}
+				budgets={
+					budgets && budgets.length > 0
+						? budgets.reduce(
+								(totalAmount, budget) =>
+									totalAmount + parseFloat(budget.amount),
+								0
+						  )
+						: 0
+				}
+			/>
 			{budgets.length > 0 && (
 				<div>
 					<div className="flex flex-col items-start my-4 sm:flex-row sm:items-center sm:justify-between">
 						<div className="my-2">
-							<h3 className="top-heading">
-								Latest Budgets
-							</h3>
+							<h3 className="top-heading">Latest Budgets</h3>
 							<p className="top-description">
 								Take a look at your latest budgets and how your fair.
 							</p>
@@ -54,23 +72,26 @@ const Dashboard = () => {
 					<div className="gap-4 grid grid-cols-1 sm:gap-5 md:gap-6 md:grid-cols-2 lg:gap-8">
 						{budgets.slice(0, 3).map((budget, index) => (
 							<div key={index}>
-								<BudgetCard {...budget} 
+								<BudgetCard
+									{...budget}
 									updateBudget={(value) => {
-										dispatch(open())
-										navigate(BUDGETS_PAGE_URL, {state: {
-											budgetValue: value
-										}})
+										dispatch(open());
+										navigate(BUDGETS_PAGE_URL, {
+											state: {
+												budgetValue: value,
+											},
+										});
 									}}
 								/>
 							</div>
 						))}
 					</div>
 					<div className="my-3 max-w-[8rem]">
-						<Button 
+						<Button
 							bg="bg-primary-500 hover:bg-primary-300"
 							caps
 							link={BUDGETS_PAGE_URL}
-							title="View all" 
+							title="View all"
 						/>
 					</div>
 				</div>
@@ -79,11 +100,10 @@ const Dashboard = () => {
 				<div>
 					<div className="flex flex-col items-start my-4 sm:flex-row sm:items-center sm:justify-between">
 						<div className="my-2">
-							<h3 className="top-heading">
-								Latest Expenses
-							</h3>
+							<h3 className="top-heading">Latest Expenses</h3>
 							<p className="top-description">
-								Take a look at your latest expenses and what you&apos;ve been doing.
+								Take a look at your latest expenses and what you&apos;ve been
+								doing.
 							</p>
 						</div>
 					</div>
@@ -95,11 +115,11 @@ const Dashboard = () => {
 						))}
 					</div>
 					<div className="my-3 max-w-[8rem]">
-						<Button 
+						<Button
 							bg="bg-primary-500 hover:bg-primary-300"
 							caps
 							link={EXPENSES_PAGE_URL}
-							title="View all" 
+							title="View all"
 						/>
 					</div>
 				</div>
@@ -107,6 +127,6 @@ const Dashboard = () => {
 			{loading && <LoadingPage />}
 		</div>
 	);
-}
+};
 
 export default Dashboard;
