3f98f0391482f6e6216b813a39dae62409864a2e
kingkite <kitewebdevelopers@gmail.com>
2022-07-08 09:47:21 +0100

    Stable a bit 1

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -26,25 +26,27 @@ const Dashboard = () => {
 		<div>		
 			<Top />
 			<Cards />
-			<div>
-				<div className="flex flex-col items-start my-4 sm:flex-row sm:items-center sm:justify-between">
-					<div className="my-2">
-						<h3 className="top-heading">
-							Latest Budgets
-						</h3>
-						<p className="top-description">
-							Take a look at your lateset budgets and how your fair.
-						</p>
+			{budgets.length > 0 && (
+				<div>
+					<div className="flex flex-col items-start my-4 sm:flex-row sm:items-center sm:justify-between">
+						<div className="my-2">
+							<h3 className="top-heading">
+								Latest Budgets
+							</h3>
+							<p className="top-description">
+								Take a look at your lateset budgets and how your fair.
+							</p>
+						</div>
+					</div>
+					<div className="gap-4 grid grid-cols-1 sm:gap-5 md:gap-6 md:grid-cols-2 lg:gap-8">
+						{budgets.slice(0, 3).map((budget, index) => (
+							<div key={index}>
+								<BudgetCard {...budget} showEditDeleteButton={false} />
+							</div>
+						))}
 					</div>
 				</div>
-				<div className="gap-4 grid grid-cols-1 sm:gap-5 md:gap-6 md:grid-cols-2 lg:gap-8">
-					{budgets.slice(0, 3).map((budget, index) => (
-						<div key={index}>
-							<BudgetCard {...budget} showEditDeleteButton={false} />
-						</div>
-					))}
-				</div>
-			</div>
+			)}
 			{loading && <LoadingPage />}
 		</div>
 	);
